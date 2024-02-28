9798b65e840351b1faf3679de2d0f29709b0b928
cyan-vm <cyan.mv@gmail.com>
2024-02-27 23:07:35 -0600

    Binary classification working with ~24 errors

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-import torch
-import torch.nn as nn 
-import torch.nn.functional as F
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
 
-class Model(nn.Module): 
-    # Four features of the flower --> 
-    # Hidden layer (number of neurons)
-    # --> H2 (n) 
-    # --> Output (3 classes of iris flowers)
-    def __init__(self, in_features=4, h1=8, h2=8):         
+# Read the data
+datos = pd.read_excel("~/Downloads/salidas.xlsx")
+
+# print(datos.head())
 
 
-        
+# Define mapping for encoding
+encoding_mapping = {
+    'Hombre': 1, 'Mujer': 0,  # Gender
+    'No': 0, 'Si': 1           # Other binary features
+}
 
+datos_encoded = datos.replace(encoding_mapping)
+
+# Randomly assign 0 or 1 to the 'depresion' feature
+datos_encoded['depresion'] = np.random.randint(0, 2, size=len(datos_encoded))
+
+x  = np.arange(10).reshape(-1, 1)
+x
+y = np.array([0, 1, 0, 0, 1, 1, 1, 1, 1, 1])
+
+
+# print(datos_encoded)
 
